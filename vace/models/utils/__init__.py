7ab1cb4152186e81ff585760566fb829b81084ce
tensae-b <tensaeb2016@gmail.com>
2025-08-06 06:22:10 +0000

    functional video to video with sections

--- vace/models/utils/__init__.py
+++ vace/models/utils/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from .preprocessor import VaceVideoProcessor
+from .preprocessor_long import VaceVideoProcessorLong
--- vace/models/wan/__init__.py
+++ vace/models/wan/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from . import modules
 from .wan_vace import WanVace
-from .framepack_vace import FramepackVace
+# from .framepack_vace import FramepackVace
+from .long_video_to_video import WanVace
